406463c1446170cc716c2cd24bcd29f4ab38821e
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-19 22:02:59 -0300

    update

--- aula11/ex01.js
+++ aula11/ex01.js
@@ -1,2 +1,6 @@
-var vel = 60.5
+var vel = 60
 console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança.`)
