b0dfc49506dad66f0140894d90cc24f9c808d6e4
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-23 18:42:56 -0300

    update

--- exercicios/ex02/script.js
+++ exercicios/ex02/script.js
@@ -3,18 +3,20 @@ function verificar_idade() {
     var nascimento = window.document.getElementById('ano').value
     var idade = ano_atual - nascimento
     var selecionado = document.querySelector('input[name="sex"]:checked').value
+    var img = window.document.createElement('img')
+    window.document.getElementById('dois').appendChild(img)
     if (idade < 1 || idade >= 130) {
         window.alert('Verifique os dados e tente novamente!')
     } else if (selecionado == 'mulher') {
         document.getElementById('txtidade').innerText = `Detectamos ${selecionado} com ${idade} anos.`
         if (idade < 12) {
-            document.querySelector('#dois img').src = 'menina.jpg'
+            img.src = 'menina.jpg'
         } else if (idade < 18) {
-            document.querySelector('#dois img').src = 'menina_adolescente.jpg'
+            img.src = 'menina_adolescente.jpg'
         } else if (idade < 60) {
-            document.querySelector('#dois img').src = 'mulher_adulta.jpg'
+            img.src = 'mulher_adulta.jpg'
         } else {
-            document.querySelector('#dois img').src = 'mulher_idosa.jpg'
+            img.src = 'menina_idosa.jpg'
         }
     } else if (selecionado == 'homem') {
         document.getElementById('txtidade').innerText = `Detectamos ${selecionado} com ${idade} anos.`
