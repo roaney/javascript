f8ff151388653d97285d682afde163a83eaf4098
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-24 15:05:34 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,3 +1,4 @@
+/*
 var c = 1
 while (c <= 6) {
     console.log('Oi')
@@ -7,3 +8,10 @@ while (c <= 10) {
     console.log(`Passo ${c}`)
     c++
 }
+*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+    
