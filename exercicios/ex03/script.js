11bf5ecef1967309df9e13cc81ba62492e416fe1
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-25 15:41:20 -0300

    Update script.js

--- exercicios/ex03/script.js
+++ exercicios/ex03/script.js
@@ -4,8 +4,17 @@ function contar() {
     var fim = Number(document.getElementById('fim').value)
     var passo = Number(document.getElementById('passo').value)
     document.getElementById('resultado').appendChild(p)
-    if (inicio == false || fim == false || passo == false) {
+    if (inicio == false || fim == false) {
         document.getElementById('txt').innerHTML = 'Impossível contar!'
+    } else if (passo == 0) {
+        window.alert('Passo inválido, considerando passo 1.')
+        passo = 1
+        document.getElementById('txt').innerHTML = 'Contando:'
+        p.innerHTML = ""
+        for (inicio; inicio <= fim; inicio += passo) {
+            p.innerHTML += `${inicio} &#128073 `   
+        }
+        p.innerHTML += '&#128308'
     } else {
         document.getElementById('txt').innerHTML = 'Contando:'
         p.innerHTML = ""
