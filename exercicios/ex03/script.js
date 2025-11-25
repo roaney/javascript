dc283255e3d09237c7e034fb19c1c908de9e1c1f
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-25 13:58:25 -0300

    Update script.js

--- exercicios/ex03/script.js
+++ exercicios/ex03/script.js
@@ -1,11 +1,13 @@
 var p = document.createElement('p')
 function contar() {
-    var inicio = document.getElementById('inicio').value
-    var fim = document.getElementById('fim').value
-    var passo = document.getElementById('passo').value
+    var inicio = Number(document.getElementById('inicio').value)
+    var fim = Number(document.getElementById('fim').value)
+    var passo = Number(document.getElementById('passo').value)
     document.getElementById('resultado').appendChild(p)
-    document.getElementById('txt').innerHTML = `Contando: ${inicio} ${fim} ${passo}`
-    /*for (inicio; inicio <= fim; inicio + 1) {
-        p.innerHTML += `${inicio} &#128073 `
-    }*/
+    document.getElementById('txt').innerHTML = 'Contando:'
+    p.innerHTML = ""
+    for (inicio; inicio <= fim; inicio += passo) {
+        p.innerHTML += `${inicio} &#128073 `   
+    }
+    p.innerHTML += '&#128308'
 }
