f1f19fe9bdf177b13123bc83d5b54a336dad6f88
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-26 10:50:29 -0300

    update

--- exercicios/ex04/script.js
+++ exercicios/ex04/script.js
@@ -1,3 +1,7 @@
-function calcular() {
-    
-}
+const botoes = document.querySelectorAll('.acao-btn');
+botoes.forEach(button => {
+    button.addEventListener('click', function(e) {
+        const valorPressionado = e.target.dataset.valor;
+        document.querySelector('div p').innerHTML = valorPressionado
+    });
+});
