b709983f599a6ef6ae8c3c0d4df231a301ac60d3
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-28 11:04:48 -0300

    Update script.js

--- exercicios/ex03/script.js
+++ exercicios/ex03/script.js
@@ -1,40 +1,45 @@
 var p = document.createElement('p')
 function contar() {
-    var inicio = Number(document.getElementById('inicio').value)
-    var fim = Number(document.getElementById('fim').value)
-    var passo = Number(document.getElementById('passo').value)
+    var inicio = document.getElementById('inicio').value
+    var fim = document.getElementById('fim').value
+    var passo = document.getElementById('passo').value
     document.getElementById('resultado').appendChild(p)
-    if (inicio == false || fim == false) {
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
         p.innerHTML = ""
         document.getElementById('txt').innerHTML = 'Impossível contar!'
-    } else if (passo == 0) {
-        window.alert('Passo inválido, considerando passo 1.')
-        passo = 1
-        document.getElementById('txt').innerHTML = 'Contando:'
-        p.innerHTML = ""
-        if (inicio <= fim) {
-            for (inicio; inicio <= fim; inicio += passo) {
-                p.innerHTML += `${inicio} &#128073 `   
-            }
-        } else {
-            for (inicio; inicio >= fim; inicio -= passo) {
-                p.innerHTML += `${inicio} &#128073 `   
-            }
-        }
-        p.innerHTML += '&#128308'
-    } else if (inicio <= fim) {
-        document.getElementById('txt').innerHTML = 'Contando:'
-        p.innerHTML = ""
-        for (inicio; inicio <= fim; inicio += passo) {
-            p.innerHTML += `${inicio} &#128073 `   
-        }
-        p.innerHTML += '&#128308'
     } else {
-        document.getElementById('txt').innerHTML = 'Contando:'
-        p.innerHTML = ""
-        for (inicio; inicio >= fim; inicio -= passo) {
-            p.innerHTML += `${inicio} &#128073 `   
+        var inicio_numero = Number(inicio)
+        var fim_numero = Number(fim)
+        var passo_numero = Number(passo)
+        if (passo_numero == 0) {
+            window.alert('Passo inválido, considerando passo 1.')
+            passo_numero = 1
+            document.getElementById('txt').innerHTML = 'Contando:'
+            p.innerHTML = ""
+            if (inicio_numero <= fim_numero) {
+                for (inicio_numero; inicio_numero <= fim_numero; inicio_numero += passo_numero) {
+                    p.innerHTML += `${inicio_numero} &#128073 `   
+                }
+            } else {
+                for (inicio_numero; inicio_numero >= fim; inicio_numero -= passo_numero) {
+                    p.innerHTML += `${inicio_numero} &#128073 `   
+                }
+            }
+            p.innerHTML += '&#128308'
+        } else if (inicio_numero <= fim_numero) {
+            document.getElementById('txt').innerHTML = 'Contando:'
+            p.innerHTML = ""
+            for (inicio_numero; inicio_numero <= fim_numero; inicio_numero += passo_numero) {
+                p.innerHTML += `${inicio_numero} &#128073 `   
+            }
+            p.innerHTML += '&#128308'
+        } else {
+            document.getElementById('txt').innerHTML = 'Contando:'
+            p.innerHTML = ""
+            for (inicio_numero; inicio_numero >= fim_numero; inicio_numero -= passo_numero) {
+                p.innerHTML += `${inicio_numero} &#128073 `   
+            }
+            p.innerHTML += '&#128308'
         }
-        p.innerHTML += '&#128308'
     }
 }
