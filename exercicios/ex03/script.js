e63e27041256aa40bbeb863aa95178f63be2cc62
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-28 11:34:39 -0300

    Update script.js

--- exercicios/ex03/script.js
+++ exercicios/ex03/script.js
@@ -1,9 +1,9 @@
 var p = document.createElement('p')
+document.getElementById('resultado').appendChild(p)
 function contar() {
     var inicio = document.getElementById('inicio').value
     var fim = document.getElementById('fim').value
     var passo = document.getElementById('passo').value
-    document.getElementById('resultado').appendChild(p)
     if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
         p.innerHTML = ""
         document.getElementById('txt').innerHTML = 'Impossível contar!'
@@ -11,11 +11,11 @@ function contar() {
         var inicio_numero = Number(inicio)
         var fim_numero = Number(fim)
         var passo_numero = Number(passo)
+        document.getElementById('txt').innerHTML = 'Contando:'
+        p.innerHTML = ""
         if (passo_numero == 0) {
             window.alert('Passo inválido, considerando passo 1.')
             passo_numero = 1
-            document.getElementById('txt').innerHTML = 'Contando:'
-            p.innerHTML = ""
             if (inicio_numero <= fim_numero) {
                 for (inicio_numero; inicio_numero <= fim_numero; inicio_numero += passo_numero) {
                     p.innerHTML += `${inicio_numero} &#128073 `   
@@ -27,15 +27,11 @@ function contar() {
             }
             p.innerHTML += '&#128308'
         } else if (inicio_numero <= fim_numero) {
-            document.getElementById('txt').innerHTML = 'Contando:'
-            p.innerHTML = ""
             for (inicio_numero; inicio_numero <= fim_numero; inicio_numero += passo_numero) {
                 p.innerHTML += `${inicio_numero} &#128073 `   
             }
             p.innerHTML += '&#128308'
         } else {
-            document.getElementById('txt').innerHTML = 'Contando:'
-            p.innerHTML = ""
             for (inicio_numero; inicio_numero >= fim_numero; inicio_numero -= passo_numero) {
                 p.innerHTML += `${inicio_numero} &#128073 `   
             }
