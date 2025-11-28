cccf313bc03580e8a826698c9da1cef2ac17e4ff
Roaney Faria Adami <roaney91@hotmail.com>
2025-11-28 15:07:57 -0300

    Update script.js

--- exercicios/ex03/script.js
+++ exercicios/ex03/script.js
@@ -4,38 +4,27 @@ function contar() {
     var inicio = document.getElementById('inicio').value
     var fim = document.getElementById('fim').value
     var passo = document.getElementById('passo').value
+    p.innerHTML = ""
     if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
-        p.innerHTML = ""
         document.getElementById('txt').innerHTML = 'Impossível contar!'
     } else {
         var inicio_numero = Number(inicio)
         var fim_numero = Number(fim)
         var passo_numero = Number(passo)
         document.getElementById('txt').innerHTML = 'Contando:'
-        p.innerHTML = ""
-        if (passo_numero == 0) {
+        if (passo_numero <= 0) {
             window.alert('Passo inválido, considerando passo 1.')
             passo_numero = 1
-            if (inicio_numero <= fim_numero) {
-                for (inicio_numero; inicio_numero <= fim_numero; inicio_numero += passo_numero) {
-                    p.innerHTML += `${inicio_numero} &#128073 `   
-                }
-            } else {
-                for (inicio_numero; inicio_numero >= fim; inicio_numero -= passo_numero) {
-                    p.innerHTML += `${inicio_numero} &#128073 `   
-                }
-            }
-            p.innerHTML += '&#128308'
-        } else if (inicio_numero <= fim_numero) {
+        }
+        if (inicio_numero <= fim_numero) {
             for (inicio_numero; inicio_numero <= fim_numero; inicio_numero += passo_numero) {
                 p.innerHTML += `${inicio_numero} &#128073 `   
             }
-            p.innerHTML += '&#128308'
         } else {
             for (inicio_numero; inicio_numero >= fim_numero; inicio_numero -= passo_numero) {
                 p.innerHTML += `${inicio_numero} &#128073 `   
             }
-            p.innerHTML += '&#128308'
         }
+        p.innerHTML += '&#128308'
     }
 }
