c28cb77c818f45eb1f38f94edc03d5f0adfec448
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-01 19:27:06 -0300

    update

---
+++ exercicios/ex05/script.js
@@ -0,0 +1,15 @@
+document.getElementById('adicionar').addEventListener('click', add)
+document.getElementById('finalizar').addEventListener('click', finish)
+let valores = []
+function add() {
+    let novo_valor = document.getElementById('numero').value
+    if (valores.indexOf(novo_valor) != -1 || novo_valor < 1 || novo_valor > 100 || novo_valor.length == 0) {
+        window.alert('Valor inválido ou já inserido na lista.')
+    } else {
+        valores.push(novo_valor)
+        document.getElementById('txtvalores').innerHTML += `Valor ${novo_valor} adicionado.<br>`
+    }
+}
+function finish() {
+    
+}
