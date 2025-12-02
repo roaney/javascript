2fcfb69ba19b25e78fd2c9602c5eecaf108cf247
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-02 14:42:42 -0300

    Update script.js

--- exercicios/ex05/script.js
+++ exercicios/ex05/script.js
@@ -2,21 +2,21 @@ document.getElementById('adicionar').addEventListener('click', add)
 document.getElementById('finalizar').addEventListener('click', finish)
 let valores = []
 function add() {
-    let novo_valor = document.getElementById('numero').value
+    let novo_valor = Number(document.getElementById('numero').value)
     if (valores.indexOf(novo_valor) != -1 || novo_valor < 1 || novo_valor > 100 || novo_valor.length == 0) {
         window.alert('Valor inválido ou já inserido na lista.')
     } else {
-        valores.push(Number(novo_valor))
+        valores.push(novo_valor)
         document.getElementById('txtvalores').innerHTML += `Valor ${novo_valor} adicionado.<br>`
     }
 }
 function finish() {
     document.getElementById('txtresultado').innerHTML = "" 
-    let results = [quant(), maior(), menor()]
+    let results = [quant(), maior(), menor(), soma()]
     results.forEach(elemento => {
         document.getElementById('txtresultado').innerHTML += elemento
     })
-    //quant(); maior(); menor(); soma(); media()
+    //soma(); media()
 }
 function quant() {
     return `Ao todo, temos ${valores.length} números cadastrados.<br>`
@@ -24,7 +24,7 @@ function quant() {
 function maior() {
     let maior_valor = valores[0]
     valores.forEach(elemento => {
-        if (Number(elemento) > Number(maior_valor)) {
+        if (elemento > maior_valor) {
             maior_valor = elemento
         }
     })
@@ -33,9 +33,16 @@ function maior() {
 function menor() {
     let menor_valor = valores[0]
     valores.forEach(elemento => {
-        if (Number(elemento) < Number(menor_valor)) {
+        if (elemento < menor_valor) {
             menor_valor = elemento
         }
     })
     return `O menor valor informado foi ${menor_valor}.<br>`
+}
+function soma() {
+    let soma_valores = 0
+    valores.forEach(elemento => {
+        soma_valores += elemento
+    })
+    return `Somando todos os valores, temos ${soma_valores}.`
 }
