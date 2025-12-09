7e53f0a5872851d1a8aed914e8bf8fa5834c826a
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-09 11:17:54 -0300

    Update arrow02.js

--- exercicios/ex06/arrow02.js
+++ exercicios/ex06/arrow02.js
@@ -1,18 +1,18 @@
 function Temporizador() {
-    this.segundos = 0;
+    this.segundos = 0
     // Função tradicional em setTimeout:
     // 'this' aqui dentro apontaria para o objeto 'global'
     // (ou undefined em modo estrito), NÃO para o objeto Temporizador.
     // setTimeout(function() {
-    //   this.segundos++; // Erro/Comportamento inesperado
-    // }, 1000);
+    //     this.segundos++ // Erro/Comportamento inesperado
+    // }, 1000)
     // Arrow Function em setTimeout:
     // 'this' herda o 'this' da função Temporizador.
     // 'this.segundos' refere-se corretamente à propriedade do objeto Temporizador.
     setTimeout(() => {
-        this.segundos++; 
-        console.log(`Passou 1 segundo. Segundos: ${this.segundos}`);
-    }, 1000);
+        this.segundos++
+        console.log(`Passou 1 segundo. Segundos: ${this.segundos}`)
+    }, 1000)
 }
-const tempo = new Temporizador();
+const tempo = new Temporizador()
 // (Se executarmos, 'tempo.segundos' será 1 após 1 segundo)
