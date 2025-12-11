7c97636c47648f965265c690d92f291a54a90586
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-11 11:50:21 -0300

    Update map.js

--- exercicios/ex06/map.js
+++ exercicios/ex06/map.js
@@ -2,3 +2,12 @@ const numeros = [1, 2, 3, 4, 5]
 // Usando Arrow Function e Retorno Implícito
 const dobrados = numeros.map(numero => numero * 2)
 console.log(dobrados); // [2, 4, 6, 8, 10]
+
+/*
+const numeros = [1, 2, 3, 4, 5]
+const dobrados = []
+numeros.forEach(numero => {
+    dobrados.push(numero * 2)
+})
+console.log(dobrados)
+*/
