7c01afff502111d6e17ad95eeae8315607977405
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-12 11:43:09 -0300

    Create desafio_compras.js

---
+++ exercicios/ex06/desafio_compras.js
@@ -0,0 +1,19 @@
+const carrinho = [
+    { nome: 'Maçã', categoria: 'Alimento', preco: 5 },
+    { nome: 'Mouse', categoria: 'Eletrônicos', preco: 20 },
+    { nome: 'Pão', categoria: 'Alimento', preco: 3 },
+    { nome: 'Monitor', categoria: 'Eletrônicos', preco: 150 },
+    { nome: 'Leite', categoria: 'Alimento', preco: 4 }
+]
+
+const alimentos = carrinho.filter(item => item.categoria == 'Alimento')
+console.log(alimentos)
+const taxa = alimentos.map(item => item.preco += 2)
+console.log(taxa)
+const total = taxa.reduce((total, item_atual) => total + item_atual, 0)
+console.log(total)
+
+/*
+    const total = carrinho.filter(item => item.categoria == 'Alimento').map(item => item.preco += 2).reduce((total, item_atual) => total + item_atual, 0)
+    console.log(total)
+*/
