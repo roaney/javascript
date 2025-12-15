cebe8c034183a3f879f160d9a7a6b6a270059dd0
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-15 11:38:12 -0300

    Create obj_obj.js

---
+++ exercicios/ex06/obj_obj.js
@@ -0,0 +1,11 @@
+const objt1 = {
+    objt2: {id: 1, name: 'A'},
+    objt3: {id: 2, name: 'B'}
+}
+console.log(typeof(objt1))
+console.log(typeof(objt1.objt2))
+console.log(typeof(objt1.objt3))
+console.log(typeof(objt1.objt2.id))
+console.log(typeof(objt1.objt2.name))
+console.log(typeof(objt1.objt3.id))
+console.log(typeof(objt1.objt3.name))
