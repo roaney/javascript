0d61ef481ec29c975ca64e53bb1018bd9b5019ec
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-15 18:22:54 -0300

    Update script.js

--- exercicios/ex06/dom/script.js
+++ exercicios/ex06/dom/script.js
@@ -1,3 +1,10 @@
 const itensDoCarrinho = document.querySelectorAll('.produto-carrinho')
 const [item1, item2, item3] = itensDoCarrinho
 item3.style.color = 'red'
+/*
+item1.addEventListener('click', function() {
+    return alert('Clicou!')
+})
+*/
+item1.addEventListener('click', () => alert('Clicou!'))
+item1.style.cursor = 'pointer'
