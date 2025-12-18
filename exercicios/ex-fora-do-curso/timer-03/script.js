7f1cb779172f7a81e289f44eddd1936c9d529273
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-18 15:00:44 -0300

    update

--- exercicios/ex-fora-do-curso/timer-03/script.js
+++ exercicios/ex-fora-do-curso/timer-03/script.js
@@ -1,1 +1,9 @@
-const contador = setInterval(() => console.log("Tic..."), 1000)
+const h1 = document.querySelector('h1')
+const contador = setInterval(() => h1.textContent += 'Tic... ', 1000)
+const botao = document.getElementById('parar')
+botao.addEventListener('click', () => clearInterval(contador))
+/*
+    const contador = setInterval(() => console.log("Tic..."), 1000)
+    const botao = document.getElementById('parar')
+    botao.addEventListener('click', () => clearInterval(contador))
+*/
