283f4ffc7623645a3be82819afd316124dfa3d3c
Roaney Faria Adami <roaney91@hotmail.com>
2025-12-21 15:29:05 -0300

    update

---
+++ exercicios/ex-fora-do-curso/fetch-02/script.js
@@ -0,0 +1,13 @@
+async function buscarPreco() {
+    try {
+        const resposta = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+        const preco = await resposta.json()
+        const h1 = document.getElementById('cotacao')
+        h1.textContent = preco.bitcoin.usd
+    }
+    catch(erro) {
+        alert(erro)
+    }
+}
+setInterval(() => buscarPreco(), 10000)
+//buscarPreco()
